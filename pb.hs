b185f0478ce850efe0d1bccc109964f06814b9fb
Nathan Howell <nhowell@alphaheavy.com>
2012-05-20 23:55:53 -0700

    ...

--- pb.hs
+++ pb.hs
@@ -4,14 +4,18 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverlappingInstances #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Pb where
 
 import Control.Applicative
+import Control.Monad
 import Data.Bits
 import Data.ByteString
 import Data.HashMap.Strict as HashMap
@@ -19,6 +23,7 @@ import Data.Hex
 import Data.Int
 import Data.Monoid
 import Data.Serialize.Get
+import Data.Serialize.Put
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.TypeLevel as Tl
@@ -64,6 +69,30 @@ getVarInt = go 0 0 where
       then go (n+7) (val .|. ((fromIntegral (b .&. 0x7F)) `shiftL` n))
       else return $! val .|. ((fromIntegral b) `shiftL` n)
 
+-- This can be used on any Integral type and is needed for signed types; unsigned can use putVarUInt below.
+-- This has been changed to handle only up to 64 bit integral values (to match documentation).
+{-# INLINE putVarSInt #-}
+putVarSInt :: (Integral a, Bits a) => a -> Put
+putVarSInt bIn =
+  case compare bIn 0 of
+    LT -> let -- upcast to 64 bit to match documentation of 10 bytes for all negative values
+              b = fromIntegral bIn
+              len = 10                                -- (pred 10)*7 < 64 <= 10*7
+              last'Mask = 1                           -- pred (1 `shiftL` 1)
+              go :: Int64 -> Int -> Put
+              go !i 1 = putWord8 (fromIntegral (i .&. last'Mask))
+              go !i n = putWord8 (fromIntegral (i .&. 0x7F) .|. 0x80) >> go (i `shiftR` 7) (pred n)
+          in go b len
+    EQ -> putWord8 0
+    GT -> putVarUInt bIn
+
+-- This should be used on unsigned Integral types only (not checked)
+{-# INLINE putVarUInt #-}
+putVarUInt :: (Integral a, Bits a) => a -> Put
+putVarUInt i
+  | i < 0x80  = putWord8 (fromIntegral i)
+  | otherwise = putWord8 (fromIntegral (i .&. 0x7F) .|. 0x80) >> putVarUInt (i `shiftR` 7)
+
 fieldTag :: Field -> Tag
 fieldTag f = case f of
   VarintField    t _ -> t
@@ -74,7 +103,7 @@ fieldTag f = case f of
   Fixed32Field   t _ -> t
 
 decodeMessage :: (Decode (Rep a), Generic a) => Get a
-decodeMessage = fmap (to . decode) $ go HashMap.empty where
+decodeMessage = fmap to (decode =<< go HashMap.empty) where
   go msg = do
     mfield <- Just `fmap` getField <|> return Nothing
     case mfield of
@@ -83,8 +112,8 @@ decodeMessage = fmap (to . decode) $ go HashMap.empty where
 
 class Wire a where
   decodeWire :: Field -> a
-  -- encodeWire :: Tag -> a -> Put
-  -- sizeWire   :: Tag -> a -> Int
+  encodeWire :: Tag -> a -> Put
+  sizeWire   :: a -> Int
 
 deriving instance Wire a => Wire (First a)
 deriving instance Wire a => Wire (Last a)
@@ -93,9 +122,12 @@ deriving instance Wire a => Wire (Sum a)
 
 instance Wire a => Wire (Maybe a) where
   decodeWire = Just . decodeWire
+  encodeWire t (Just a) = encodeWire t a
+  encodeWire t Nothing  = return ()
 
 instance Wire Int32 where
   decodeWire (VarintField  _ val) = fromIntegral val
+  encodeWire t val = putVarSInt val
 
 instance Wire Int64 where
   decodeWire (VarintField  _ val) = fromIntegral val
@@ -138,6 +170,10 @@ instance Wire ByteString where
 
 instance Wire T.Text where
   decodeWire (DelimitedField _ bs) = T.decodeUtf8 bs
+
+instance (Decode (Rep a), Generic a) => Wire a where
+  decodeWire (DelimitedField _ bs) = val where
+    Right val = runGet decodeMessage bs
 
 -- field rules
 newtype Required n a = Required a
@@ -227,16 +263,22 @@ deriving instance RealFrac a => RealFrac (Fixed a)
 deriving instance Show a => Show (Fixed a)
 
 class Decode (f :: * -> *) where
-  decode :: HashMap Tag [Field] -> f a
+  decode :: (Applicative m, Monad m) => HashMap Tag [Field] -> m (f a)
 
 instance Decode a => Decode (M1 i c a) where
-  decode = M1 . decode
+  decode = fmap M1 . decode
 
 instance (Wire a, Monoid a, Tl.Nat n) => Decode (K1 i (Optional n a)) where
-  decode = K1 . Optional . mconcat . fmap decodeWire . HashMap.lookupDefault [] (fromIntegral (Tl.toInt (undefined :: n)))
+  decode msg =
+    pure $! case HashMap.lookup (fromIntegral (Tl.toInt (undefined :: n))) msg of
+      Just val -> K1 . Optional . mconcat $ fmap decodeWire val
+      Nothing  -> K1 mempty
 
 instance (Wire a, Monoid a, Tl.Nat n) => Decode (K1 i (Required n a)) where
-  decode = K1 . Required . mconcat . fmap decodeWire . HashMap.lookupDefault (error "required field not found") (fromIntegral (Tl.toInt (undefined :: n)))
+  decode msg =
+    case HashMap.lookup (fromIntegral (Tl.toInt (undefined :: n))) msg of
+      Just val -> pure . K1 . Required . mconcat $ fmap decodeWire val
+      Nothing  -> fail "required field not found"
 
 {-
 instance (Wire a, Monoid a, Tl.Nat n) => Decode (K1 i (Packed n a)) where
@@ -244,7 +286,7 @@ instance (Wire a, Monoid a, Tl.Nat n) => Decode (K1 i (Packed n a)) where
 -}
 
 instance (Decode a, Decode b) => Decode (a :*: b) where
-  decode msg = decode msg :*: decode msg
+  decode msg = liftA2 (:*:) (decode msg) (decode msg)
 
 -- Taken from google's code, but I had to explcitly add fromIntegral in the right places:
 zzEncode32 :: Int32 -> Word32
@@ -259,17 +301,17 @@ zzDecode64 w = (fromIntegral (w `shiftR` 1)) `xor` (negate (fromIntegral (w .&. 
 data TestRec = TestRec
   { field1 :: Required Tl.D1 (Last Int64)
   , field2 :: Optional Tl.D2 (Last T.Text)
-  , field3 :: Optional Tl.D3 (Last Int64)
+  , field3 :: Optional Tl.D3 (Last Int32)
   -- , field3 :: Optional Tl.D3 (Signed Int)
   -- , field4 :: Optional Tl.D4 Double
   } deriving (Generic, Show)
 
 main :: IO ()
 main = do
-  let val1, val2, val3 :: TestRec
-      Right val1 = runGet decodeMessage =<< unhex "089601120774657374696e67"
-      Right val2 = runGet decodeMessage =<< unhex "089601189701"
-      Right val3 = runGet decodeMessage =<< unhex "089601"
+  let val1, val2, val3 :: Either String TestRec
+      val1 = runGet decodeMessage =<< unhex "089601120774657374696e67"
+      val2 = runGet decodeMessage =<< unhex "089601189701"
+      val3 = runGet decodeMessage =<< unhex "089601"
   print val1
   print val2
   print val3
