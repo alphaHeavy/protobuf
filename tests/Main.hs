59fb4fab36feeb10962ddd21554fef55d1983827
Nathan Howell <nhowell@alphaheavy.com>
2013-01-15 23:59:26 -0800

    Expanded test coverage, tests now fail

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
-import Test.QuickCheck.Monadic
+import Test.QuickCheck.Modifiers
 
 import GHC.Generics (Generic)
 
@@ -16,7 +20,7 @@ import Data.List
 import Data.Monoid
 import Data.Serialize
 import Data.Word
-import Data.TypeLevel.Num (D1)
+import Data.TypeLevel.Num (Nat, reifyIntegral)
 
 main :: IO ()
 main = defaultMain tests
@@ -26,36 +30,62 @@ tests =
   ]
 
 singleValueTests =
-  [ testProperty "Int32" prop_int32
-  , testProperty "Int64" prop_int64
+  [ testProperty "Int32"  prop_int32
+  , testProperty "Int64"  prop_int64
   , testProperty "Word32" prop_word32
   , testProperty "Word64" prop_word64
+  , testProperty "Float"  prop_float
+  , testProperty "Double" prop_double
   ]
 
-data OneValue a = OneValue (Required D1 (Last a))
+data OneValue n a = OneValue (Required n (Last a))
   deriving (Eq, Generic)
 
-instance EncodeWire a => Encode (OneValue a)
-instance DecodeWire a => Decode (OneValue a)
+instance (EncodeWire a, Nat n) => Encode (OneValue n a)
+instance (DecodeWire a, Nat n) => Decode (OneValue n a)
 
+prop_roundtrip :: (Eq a, Nat n, Encode (OneValue n a), Decode (OneValue n a)) => OneValue n a -> Gen Bool
 prop_roundtrip msg = do
   let bs = runPut $ encodeMessage msg
   case runGet decodeMessage bs of
     Right msg' -> return $ msg == msg'
     Left err   -> fail err
 
+prop_reify :: forall a r . NonNegative Int32 -> Last a -> (forall n . Nat n => OneValue n a -> r) -> r
+prop_reify (NonNegative n) a f = reifyIntegral n g where
+  g :: forall n . Nat n => n -> r
+  g _ = f (OneValue (putValue a) :: OneValue n a)
+
+prop_word32 :: Gen Bool
 prop_word32 = do
-  val <- OneValue . putValue . Last . Just <$> arbitrary
-  prop_roundtrip (val :: OneValue Word32)
+  val <- Last . Just <$> arbitrary
+  n <- arbitrary
+  prop_reify n (val :: Last Word32) prop_roundtrip
 
+prop_word64 :: Gen Bool
 prop_word64 = do
-  val <- OneValue . putValue . Last . Just <$> arbitrary
-  prop_roundtrip (val :: OneValue Word64)
+  val <- Last . Just <$> arbitrary
+  n <- arbitrary
+  prop_reify n (val :: Last Word64) prop_roundtrip
 
+prop_int32 :: Gen Bool
 prop_int32 = do
-  val <- OneValue . putValue . Last . Just <$> arbitrary
-  prop_roundtrip (val :: OneValue Int32)
+  val <- Last . Just <$> arbitrary
+  n <- arbitrary
+  prop_reify n (val :: Last Int32) prop_roundtrip
 
+prop_int64 :: Gen Bool
 prop_int64 = do
-  val <- OneValue . putValue . Last . Just <$> arbitrary
-  prop_roundtrip (val :: OneValue Int64)
+  val <- Last . Just <$> arbitrary
+  n <- arbitrary
+  prop_reify n (val :: Last Int64) prop_roundtrip
+
+prop_float = do
+  val <- Last . Just <$> arbitrary
+  n <- arbitrary
+  prop_reify n (val :: Last Float) prop_roundtrip
+
+prop_double = do
+  val <- Last . Just <$> arbitrary
+  n <- arbitrary
+  prop_reify n (val :: Last Double) prop_roundtrip
