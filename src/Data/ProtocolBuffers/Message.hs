098c14ab85e1f419c1dcd1ada65256ef3b22028e
Nathan Howell <nhowell@alphaheavy.com>
2013-01-23 17:05:52 -0800

    Working on docs
    [ci skip]

--- src/Data/ProtocolBuffers/Message.hs
+++ src/Data/ProtocolBuffers/Message.hs
@@ -20,8 +20,27 @@ import Data.ProtocolBuffers.Encode
 import Data.ProtocolBuffers.Wire
 
 -- |
--- A newtype wrapper used to distinguish encoded messages from other field types.
--- These messages are stored as delimited fields.
+-- The way to embed a message within another message.
+-- These embedded messages are stored as length-delimited fields.
+--
+-- For example:
+--
+-- @
+--data Inner = Inner
+--   { innerField :: 'Data.ProtocolBuffers.Required' 'Data.TypeLevel.D1' ('Data.Monoid.Last' 'Data.Int.Int64')
+--   } deriving ('GHC.Generics.Generic', 'Prelude.Show')
+--
+-- instance 'Encode' Inner
+--instance 'Decode' Inner
+--
+-- data Outer = Outer
+--   { outerField :: 'Data.ProtocolBuffers.Required' 'Data.TypeLevel.D1' ('Data.ProtocolBuffers.Message' Inner)
+--   } deriving ('GHC.Generics.Generic', 'Prelude.Show')
+--
+-- instance 'Encode' Outer
+--instance 'Decode' Outer
+-- @
+--
 newtype Message m = Message (Maybe m)
   deriving (Eq, Foldable, Functor, NFData, Ord, Show, Traversable)
 
