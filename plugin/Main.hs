88576317461de83bc116cd268c9e9046e404bfbb
Nathan Howell <nhowell@alphaheavy.com>
2013-02-02 14:24:23 -0800

    Support more plugin request decoding...

--- plugin/Main.hs
+++ plugin/Main.hs
@@ -15,9 +15,15 @@ import Data.ProtocolBuffers.Internal
 import Data.Serialize
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as TextL
+import qualified Data.Text.Lazy.Encoding as TextL
+import qualified Data.Text.Lazy.IO as TextL
 import Debug.Trace
 import qualified Data.TypeLevel as Tl
 import GHC.Generics
+import Language.Haskell.Exts
+import Language.Haskell.Exts.Pretty (prettyPrint)
+import Language.Haskell.Exts.SrcLoc (noLoc)
 
 data FieldDescriptorProto_Type
   = TYPE_DOUBLE
@@ -92,23 +98,32 @@ instance Enum FieldDescriptorProto_Label where
     fromEnum LABEL_REQUIRED      = 2
     fromEnum LABEL_REPEATED      = 3
 
+data FieldOptions = FieldOptions
+  { fieldOptionsPacked     :: Optional Tl.D2 (Last Bool)
+  , fieldOptionsDeprecated :: Optional Tl.D3 (Last Bool)
+  , fieldOptionsLazy       :: Optional Tl.D5 (Last Bool) -- disable strict field?
+  -- optional CType ctype = 1 [default = STRING];
+  } deriving (Generic, Show)
+
+instance Decode FieldOptions
+
 data FieldDescriptorProto = FieldDescriptorProto
-  { fieldDescriptorName :: Optional Tl.D1 Text
-  , fieldDescriptorNumber :: Optional Tl.D3 (Last Int32)
-  , fieldDescriptorLabel :: Optional Tl.D4 (Enumeration (Maybe FieldDescriptorProto_Label))
-  , fieldDescriptorType :: Optional Tl.D5 (Enumeration (Maybe FieldDescriptorProto_Type))
-  , fieldDescriptorTypeName :: Optional Tl.D6 Text
-  , fieldDescriptorExtendee :: Optional Tl.D2 Text
+  { fieldDescriptorName         :: Optional Tl.D1 Text
+  , fieldDescriptorNumber       :: Optional Tl.D3 (Last Int32)
+  , fieldDescriptorLabel        :: Optional Tl.D4 (Enumeration (Maybe FieldDescriptorProto_Label))
+  , fieldDescriptorType         :: Optional Tl.D5 (Enumeration (Maybe FieldDescriptorProto_Type))
+  , fieldDescriptorTypeName     :: Optional Tl.D6 Text
+  , fieldDescriptorExtendee     :: Optional Tl.D2 Text
   , fieldDescriptorDefaultValue :: Optional Tl.D7 Text
-  -- optional FieldOptions options = 8;
+  , fieldDescriptorOptions      :: Optional Tl.D8 (Message FieldOptions)
   } deriving (Generic, Show)
 
 instance Decode FieldDescriptorProto
 
 data DescriptorProto = DescriptorProto
-  { descriptorName :: Optional Tl.D1 Text
-  , descriptorField :: Repeated Tl.D2 (Message FieldDescriptorProto)
-  , descriptorExtension :: Repeated Tl.D6 (Message FieldDescriptorProto)
+  { descriptorName       :: Optional Tl.D1 Text
+  , descriptorField      :: Repeated Tl.D2 (Message FieldDescriptorProto)
+  , descriptorExtension  :: Repeated Tl.D6 (Message FieldDescriptorProto)
   , descriptorNestedType :: Repeated Tl.D3 (Message DescriptorProto)
   -- repeated EnumDescriptorProto enum_type = 4;
 
@@ -124,17 +139,17 @@ data DescriptorProto = DescriptorProto
 instance Decode DescriptorProto
 
 data FileDescriptorProto = FileDescriptorProto
-  { fileDescriptorName :: Optional Tl.D1 Text
-  , fileDescriptorPackage :: Optional Tl.D2 Text
-  , fileDescriptorDependency :: Repeated Tl.D3 Text
+  { fileDescriptorName             :: Optional Tl.D1 Text
+  , fileDescriptorPackage          :: Optional Tl.D2 Text
+  , fileDescriptorDependency       :: Repeated Tl.D3 Text
   , fileDescriptorPublicDependency :: Repeated Tl.D10 Int32
-  , fileDescriptorWeakDependency :: Repeated Tl.D11 Int32
+  , fileDescriptorWeakDependency   :: Repeated Tl.D11 Int32
 
   -- All top-level definitions in this file.
-  , fileDescriptorMessageType :: Repeated Tl.D4 (Message DescriptorProto)
+  , fileDescriptorMessageType      :: Repeated Tl.D4 (Message DescriptorProto)
   -- repeated EnumDescriptorProto enum_type = 5;
   -- repeated ServiceDescriptorProto service = 6;
-  , fileDescriptorExtension :: Repeated Tl.D7 (Message FieldDescriptorProto)
+  , fileDescriptorExtension        :: Repeated Tl.D7 (Message FieldDescriptorProto)
 
   -- optional FileOptions options = 8;
 
@@ -149,36 +164,56 @@ instance Decode FileDescriptorProto
 
 data CodeGeneratorRequest = CodeGeneratorRequest
   { fileToGenerate :: Repeated Tl.D1 Text
-  , parameter :: Optional Tl.D2 Text
-  , protoFile :: Repeated Tl.D15 (Message FileDescriptorProto)
+  , parameter      :: Optional Tl.D2 Text
+  , protoFile      :: Repeated Tl.D15 (Message FileDescriptorProto)
   } deriving (Generic, Show)
 
 instance Decode CodeGeneratorRequest
 
 data CodeGeneratorResponse_File = CodeGeneratorResponse_File
-  { responseFileName :: Optional Tl.D1 Text
+  { responseFileName       :: Optional Tl.D1 Text
   , responseInsertionPoint :: Optional Tl.D2 Text
-  , responseContent :: Optional Tl.D15 Text
+  , responseContent        :: Optional Tl.D15 Text
   } deriving (Generic, Show)
 
 instance Encode CodeGeneratorResponse_File
 
 data CodeGeneratorResponse = CodeGeneratorResponse
-  { errorStr :: Optional Tl.D1 Text
+  { errorStr :: Optional Tl.D1 String
   , responseFiles :: Repeated Tl.D15 (Message CodeGeneratorResponse_File)
   } deriving (Generic, Show)
 
 instance Encode CodeGeneratorResponse
 
+blah :: FileDescriptorProto -> Text
+blah fdp = T.pack $ prettyPrint m where
+  m = Module noLoc (ModuleName "Foo") pragmas Nothing exports imports decls
+  pragmas = [LanguagePragma noLoc [Ident "DataKinds"]]
+  exports = Nothing
+  imports = []
+  decls = []
+
+blahs :: [Message FileDescriptorProto] -> [CodeGeneratorResponse_File]
+blahs = fmap step where
+  step (Message (Just fpd)) = CodeGeneratorResponse_File
+    { responseFileName = putValue "test.hs"
+    , responseInsertionPoint = putValue ""
+    , responseContent = putValue $ blah fpd
+    }
+
 -- getCodeFor :: HashMap Tag [Field] -> Bl.ByteString
 getCodeFor :: CodeGeneratorRequest -> CodeGeneratorResponse
 getCodeFor val = traceShow val CodeGeneratorResponse
-  { errorStr = putValue "some failure string"
-  , responseFiles = putValue []
+  { errorStr = putValue "" -- some failure string"
+  , responseFiles = putValue . fmap (Message . Just) . blahs . getValue $ protoFile val
   }
 
 main :: IO ()
 main = Bl.interact $ \ input ->
-  -- (HashMap Tag [Field])
-  case runGetLazy decodeMessage input of
-    Right val -> runPutLazy (encodeMessage (getCodeFor val))
+  runPutLazy . encodeMessage $
+    case runGetLazy decodeMessage input of
+      Right val -> getCodeFor val
+      Left  err -> CodeGeneratorResponse
+       { errorStr = putValue err
+       , responseFiles = putValue []
+       }
