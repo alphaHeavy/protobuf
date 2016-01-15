415f1e63dcea15af3c508d014bc5e323d42eac77
RyanGlScott <ryan.gl.scott@gmail.com>
2016-01-14 22:19:05 -0500

    Import orphan Foldable Last instance from base-orphans

--- src/Data/ProtocolBuffers/Orphans.hs
+++ src/Data/ProtocolBuffers/Orphans.hs
@@ -1,7 +1,3 @@
-{-# LANGUAGE DeriveFoldable #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
-
 -- |
 -- Messages containing 'Optional' 'Enumeration' fields fail to encode.
 -- This module contains orphan instances required to make these functional.
@@ -11,7 +7,4 @@
 
 module Data.ProtocolBuffers.Orphans (Foldable) where
 
-import Data.Foldable (Foldable)
-import Data.Monoid (Last(..))
-
-deriving instance Foldable Last
+import Data.Orphans ()
